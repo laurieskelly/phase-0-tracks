fa365b1579472da4015744aaed87ba379cabd496
Laurie Skelly <laurieskelly@gmail.com>
2016-05-29 21:22:05 -0500

    finished through release 5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -55,20 +55,49 @@ end
 
 ### DRIVER CODE 
 
-encrypted = encrypt('yo mama')
-puts "answer: #{encrypted}"
+### RELEASE 5: INTERFACE
 
-decrypted = decrypt("zp nbnb")
-puts "answer: #{decrypted}"
+# Ask user whether they want to encrypt or decrypt i.e. which method to call
+puts "Would you like to decrypt or encrypt a password?"
 
-puts encrypt("abc")
-puts encrypt("zed")
-puts decrypt("bcd")
-puts decrypt("afe")
+# validate input
+valid = false
+while valid == false
+	
+	choice = gets.chomp
+	if ['encrypt','decrypt'].include? choice
+		valid = true
+	else
+		puts "Please input either 'encrypt' or 'decrypt' in lower case (no quotes)."
+	end
+end
 
-puts "\nNested call! "
+# Ask user for password, which will be string input for encrypt/decrypt method
+puts "Enter the string you would like to #{choice}"
+password = gets.chomp
 
-puts "answer: #{decrypt(encrypt("swordfish"))}"
+# Call appropriate method and prints results 
+# Jessica and I googled and found this sweet built-in method `send()` to make this step super simple and snazzy.
+p send(choice,password) 
+
+
+
+### RELEASE 2,3,4 TEST CODE
+
+# encrypted = encrypt('yo mama')
+# puts "answer: #{encrypted}"
+
+# decrypted = decrypt("zp nbnb")
+# puts "answer: #{decrypted}"
+
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
+
+# puts "\nNested call! "
+
+# puts "answer: #{decrypt(encrypt("swordfish"))}"
 
 # This nested call works because ruby interprets the calls from the inside
 # out. i.e. - it encrypts "swordfish" and returns the encrypted 
