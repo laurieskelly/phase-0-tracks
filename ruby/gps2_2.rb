f5e4d6a5da4ccc11b473b42d64f4f9651bdd198b
Laurie Skelly <laurieskelly@gmail.com>
2016-07-06 16:44:41 -0500

    gps2_2.rb after phase release 1 pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,38 @@
+# Method to create a list
+# input: string of (one-word) items separated by spaces 
+# steps: 
+  # parse string into pieces
+  # set default quantity to nil 
+  # print the list to the console 
+# output: array of 2-item arrays [item, quantity]
+
+# Method to add an item to a list
+# input: existing list, item name, and optional quantity
+# steps: 
+	# format list item 
+	# append to list
+# output: updated list
+
+# Method to remove an item from the list
+# input: item name
+# steps: 
+	# check for item
+	# alert if not found
+	# remove item from list
+# output: updated list
+
+# Method to update the quantity of an item
+# input: list, item name, new quantity
+# steps:
+	# check for item
+	# alert if item not found
+	# update quantity
+# output: updated list
+
+# Method to print a list and make it look pretty
+# input: list
+# steps: 
+	# init format string
+	# iterate over list
+		# print each item to screen using format string
+# output: none
