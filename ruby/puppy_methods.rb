b4f58374a07e6dfac02da6ad0c95b11651b47be5
Laurie Skelly <laurieskelly@gmail.com>
2016-07-06 23:30:50 -0500

    6.2 release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,13 +28,14 @@ class Puppy
 
 	def stay(seconds)
 		counter = 0
+		speak(1)
 		while counter < seconds
-			speak(1)
 			sleep 1
 			counter += 1
 			puts counter
 		end
 		speak(1)
+		sleep 1
 	end
 end
 
@@ -50,4 +51,40 @@ pup.roll_over
 puts "\n3 in human years is how many dog years"
 pup.dog_years(3)
 puts "\nstay for 5 seconds"
-p.stay(5)
+pup.stay(5)
+
+class Student
+
+	def initialize
+		puts "Initializing a new student..."
+	end
+
+	def complain_about_depth
+		puts "I really wish we had gone into some of the topics deeper"
+	end
+
+	def complain_about_fast
+		puts "The pace of the class was crazy fast"
+	end
+
+	def complain_about_slow
+		puts "The pace of the class was embarrassingly slow for what was advertised as a rigorous curriculum"
+	end
+
+	def get_job
+		puts "The class was the best experience of my life and my instructors were amazing"
+	end
+
+end
+
+1.upto(50)  do |i|
+	puts "\nNew student \##{i}"
+	stud = Student.new
+	puts stud
+	puts 
+	stud.complain_about_depth
+	stud.complain_about_fast
+	stud.complain_about_slow
+	stud.get_job
+end
+
