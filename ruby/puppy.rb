d6823a80562e0f087dde9d70824f75552fadd18d
Laurie Skelly <laurieskelly@gmail.com>
2016-07-06 23:04:34 -0500

    puppy class spec

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Fur color: varies
+# Friendly: 0-10
+# Name: varies
+# Color: varies
+# Obedience: 0-10
+# Tricks: [can_sit=false, can_shake_hands=false]
+
+# BEHAVIOR -----------------------
+# Sit
+# Shake hands
+# Eat treat
